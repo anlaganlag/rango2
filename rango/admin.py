fc8018853a937a07cc4edc767d8775b3c08c6751
gal <tanjhu@163.com>
2020-01-01 13:23:46 +0800

    second_branch

---
+++ rango/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from rango.models import Category,Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category','url')
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','likes','views','slug')
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Page,PageAdmin)
+
